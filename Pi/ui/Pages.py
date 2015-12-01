9fca4b25a1d6cb135e1743538993f1214dcbdcc7
bturi01 <bturi01@bellarmine.edu>
2015-11-30 20:39:08 -0500

    Create Pages.py

---
+++ Pi/ui/Pages.py
@@ -0,0 +1,15 @@
+from gi.repository import Gtk
+
+
+class DistortionEffectPage(Gtk.Grid):
+    def __init__(self):
+        super(DistortionEffectPage, self).__init__()
+
+class ReverbEffectPage(Gtk.Grid):
+    def __init__(self):
+        super(ReverbEffectPage, self).__init__()
+
+
+class DelayEffectPage(Gtk.Grid):
+    def __init__(self):
+        super(DelayEffectPage, self).__init__()
