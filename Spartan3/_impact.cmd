570415582cf0c0554b712e25957c9c5dbb210638
unknown <bturi01@bellarmine.edu>
2015-11-30 22:21:54 -0500

    Updated Project files

---
+++ Spartan3/_impact.cmd
@@ -0,0 +1,48 @@
+setPreference -pref UserLevel:NOVICE
+setPreference -pref MessageLevel:DETAILED
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref ConfigOnFailure:STOP
+setPreference -pref StartupCLock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref svfUseTime:FALSE
+setPreference -pref UserLevel:NOVICE
+setPreference -pref MessageLevel:DETAILED
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref ConfigOnFailure:STOP
+setPreference -pref StartupCLock:AUTO_CORRECTION
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref svfUseTime:FALSE
+setMode -bs
+setPreference -pref UserLevel:Novice
+setMode -pff
+setMode -mpm
+setMode -cf
+setMode -dtconfig
+setMode -bsfile
+setMode -sm
+setMode -ss
+setMode -bs
+addDevice -position 1 -file "C:\MidiSynth\top_level.bit"
+
+addDevice -position 2 -part "xcf02s"
+setCable -port lpt1
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -bsfile
+setMode -dtconfig
+setMode -cf
+setMode -mpm
+setMode -pff
+setMode -bs
+Program -p 1 
+Program -p 1 
+setAttribute -position 1 -attr configFileName -value "C:\MidiSynth\i2s_cntrl.bit"
+Program -p 1 
+setAttribute -position 1 -attr configFileName -value "C:\MidiSynth\top_level.bit"
+Program -p 1 
